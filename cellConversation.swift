43454b7fb47688cef040ed428a31f9e9ba244b90
jay-lad <12mscca05@gmail.com>
2017-03-03 10:08:11 +0530

    string to string chat implement

---
+++ cellConversation.swift
@@ -0,0 +1,24 @@
+//
+//  cellConversation.swift
+//  MySampleApp
+//
+//  Created by Jay Lad on 02/03/17.
+//
+//
+
+import UIKit
+
+class cellConversation: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
